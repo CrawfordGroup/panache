f031a9c10dcf7524f47a54c3fcfd1efe876098b6
Ben Pritchard <ben@bennyp.org>
2014-07-16 16:53:15 -0400

    Fix ERD in parallel

--- scripts/runalltests.sh
+++ scripts/runalltests.sh
@@ -2,7 +2,42 @@
 
 RUNTESTSH=$1
 
-for R in *intel* *gcc* *clang*
+(
+for I in *gcc49* *clang-lapack*
 do
-  ${RUNTESTSH} ${R}/bin/runtest
+  ${RUNTESTSH} ${I}/bin/runtest
 done
+)
+
+
+. /opt/tag/gcc-4.8.3.tag
+
+(
+
+for I in *gcc48*
+do
+  ${RUNTESTSH} ${I}/bin/runtest
+done
+)
+
+
+
+(
+. /opt/tag/intel-2013-sp1-update3.tag
+
+for I in *intel14*
+do
+  ${RUNTESTSH} ${I}/bin/runtest
+done
+)
+
+
+(
+. /opt/tag/intel-2015-beta.tag
+
+for I in *intel15*
+do
+  ${RUNTESTSH} ${I}/bin/runtest
+done
+)
+
