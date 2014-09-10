0d5273a14fd04c42c81950a69550a5aa7e128712
Ben Pritchard <ben@bennyp.org>
2014-09-10 17:29:56 -0400

    Work with Cyclops

--- panache/DFTensor_CyclopsQTensor.cc
+++ panache/DFTensor_CyclopsQTensor.cc
@@ -18,30 +18,70 @@ void DFTensor::CyclopsQTensor::Reset_(void)
 
 void DFTensor::CyclopsQTensor::Write_(double * data, int nij, int istart, int jstart)
 {
-/*
     size_t nelements = nij*ndim12();
 
-    std::vector<long> indices(nelements);
+    std::vector<long> indices;
+    indices.reserve(nelements);
 
-    for(size_t i = 0; i < nelements; i++)
+
+    IJIterator it(ndim1(), ndim2(), packed());
+    it += calcindex(istart, jstart);
+
+    if(byq())
     {
-        if(byq())
+        for(int q = 0; q < naux(); q++)
         {
-            indices.push_back()
-        }
-        else
-        {
+            indices.push_back(q+it.i()*naux()+it.j()*naux()*ndim1());
+            ++it;
         }
     }
-*/
+    else
+    {
+        for(int q = 0; q < naux(); q++)
+        {
+            indices.push_back(it.i()+it.j()*ndim1()+q*ndim1()*ndim2());
+            ++it;
+        }
+    }
+
+    tensor_->write(nelements, indices.data(), data);
 }
 
 void DFTensor::CyclopsQTensor::WriteByQ_(double * data, int nq, int qstart)
 {
+
+
 }
 
 void DFTensor::CyclopsQTensor::Read_(double * data, int nij, int istart, int jstart)
 {
+    size_t nelements = nij*ndim12();
+
+    std::vector<long> indices;
+    indices.reserve(nelements);
+
+
+    IJIterator it(ndim1(), ndim2(), packed());
+    it += calcindex(istart, jstart);
+
+    if(byq())
+    {
+        for(int q = 0; q < naux(); q++)
+        {
+            indices.push_back(q+it.i()*naux()+it.j()*naux()*ndim1());
+            ++it;
+        }
+    }
+    else
+    {
+        for(int q = 0; q < naux(); q++)
+        {
+            indices.push_back(it.i()+it.j()*ndim1()+q*ndim1()*ndim2());
+            ++it;
+        }
+    }
+
+    tensor_->read(nelements, indices.data(), data);
 }
 
 void DFTensor::CyclopsQTensor::ReadByQ_(double * data, int nq, int qstart)
