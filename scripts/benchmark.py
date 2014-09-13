5cac12e680a3b5f3400e0b85e2bf88b991347db9
Ben Pritchard <ben@bennyp.org>
2014-09-13 17:11:50 -0400

    Work on benchmark script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -7,20 +7,28 @@ import re
 
 
 argparser = argparse.ArgumentParser()
+argparser.add_argument("-m", help="Path to molecule/basis directory to use", required=True)
+argparser.add_argument("-r", help="Path to runtest executable", required=True)
+argparser.add_argument("-o", help="Output directory", required=True)
+argparser.add_argument("-t", help="Max number of threads per process", type=int,  required=True)
+argparser.add_argument("-c", help="Test cyclops", action='store_true')
+argparser.add_argument("-M", help="Enable testing with MPI", action='store_true')
+argparser.add_argument("-n", help="Max number of processes to use (if using mpi)", type=int)
+args = argparser.parse_args()
 
 
-testdir = "/home/ben/programming/psi4/libpanache/testfiles"
-molbasis = "h2o-sto3g"
-runtestpath = "runtest/runtest"
+def RunTest(mpi, nthreads, nproc, cyclops, disk, blocksize):
+    print("NTHREADS: {} NPROC: {}".format(nthreads, nproc))
 
-ntotalproc = 1
-ntotalthread = 1
+    os.environ['OMP_NUM_THREADS'] = str(nthreads)
 
-filepath = os.path.join(testdir, molbasis)
+    cmd = []
 
+    if mpi:
+        cmd.extend(["mpirun", "-n", str(nproc)])
 
-def MPIRun(nthreads, nproc, cyclops, disk, blocksize):
-    cmd = ["mpirun", "-n", str(nproc), runtestpath, "-v"]
+    cmd.extend([args.r, "-v"])
+
 
     if(cyclops):
         cmd.append("-c")
@@ -30,14 +38,14 @@ def MPIRun(nthreads, nproc, cyclops, disk, blocksize):
     if(blocksize > 0):
         cmd.extend(["-b",str(blocksize)])
 
-    cmd.append(filepath)
+    cmd.append(args.m)
 
     outpipe = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
     output = outpipe.stdout.read().splitlines()
 
     timingline = 0
     for i in range(len(output)-1, 0, -1):
-        if(re.search(r"LibPANACHE DF Tensor Timings", output[i])):
+        if(re.search(r'LibPANACHE DF Tensor Timings', output[i])):
             timingline = i
             break
 
@@ -63,15 +71,53 @@ def MPIRun(nthreads, nproc, cyclops, disk, blocksize):
 
 results = {}
 
-for nproc in range(1,ntotalproc+1):
+nproc = 1
+if (args.M and args.n) and args.c:
+    nproc = args.n
+
+
+for nproc in range(1,nproc+1):
     nthreadres = {}
-    for nthread in range(1,ntotalthread+1):
-        resmem  = MPIRun(nthread, nproc, False, False, 0)
-        rescyc  = MPIRun(nthread, nproc, True, False, 0)
-        resdisk = MPIRun(nthread, nproc, False, True, 0)
-        nthreadres[nthread] = { 'Mem':resmem, 'Cyclops':rescyc, 'Disk':resdisk }
+    for nthread in range(1,args.t+1):
+        nthreadres[nthread] = {}
+        if nproc == 1:
+            resmem  = RunTest(args.M, nthread, nproc, False, False, 0)
+            resdisk = RunTest(args.M, nthread, nproc, False, True, 0)
+            nthreadres[nthread]['Mem'] = resmem
+            nthreadres[nthread]['Disk'] = resdisk
+        if args.c:
+            rescyc  = RunTest(args.M, nthread, nproc, True, False, 0)
+            nthreadres[nthread]['Cyclops'] = rescyc
 
     results[nproc] = nthreadres
 
-#print(results[1][1]['Cyclops']['QSO']['Gen'])
 
+
+# Generation of Qso
+column1 = '{:<12} '
+datacolumn = ' {:>13} '
+for nproc in range(1,nproc+1):
+    headstr = column1.format('Type')
+
+    if args.c:
+        cycstr = column1.format('Cyclops_{}'.format(nproc))
+
+    if nproc == 1:
+        memstr = column1.format('Memory'.format(nproc))
+        diskstr = column1.format('Disk'.format(nproc))
+
+    for nthread in range(1,args.t+1):
+        headstr += datacolumn.format('{}_Threads'.format(nthread))
+
+        if args.c:
+            cycstr += datacolumn.format(results[nproc][nthread]['Cyclops']['QSO']['Gen'])
+
+        if nproc == 1:
+            memstr += datacolumn.format(results[nproc][nthread]['Mem']['QSO']['Gen'])
+            diskstr += datacolumn.format(results[nproc][nthread]['Disk']['QSO']['Gen'])
+
+    print(headstr)
+    print(memstr)
+    print(diskstr)
+    if args.c:
+        print(cycstr)
