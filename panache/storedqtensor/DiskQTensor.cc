f3c11a0bcd066de69f64d69d001e511c3e340601
Ben Pritchard <ben@bennyp.org>
2015-01-06 15:47:39 -0500

    Small optimizations in Read/Write functions

--- panache/storedqtensor/DiskQTensor.cc
+++ panache/storedqtensor/DiskQTensor.cc
@@ -46,20 +46,24 @@ void DiskQTensor::Write_(double * data, int nij, int ijstart)
     #pragma omp critical
     #endif
     {
+        int inaux = naux();
+
         if(byq())
         {
-            for(int q = 0; q < naux(); q++)
-            {
-                file_->seekp(sizeof(double)*(q*ndim12()+ijstart), std::ios_base::beg);
+            int indim12 = ndim12();
 
-                for(int ij = 0; ij < nij; ij++)
-                    file_->write(reinterpret_cast<const char *>(data+ij*naux()+q), sizeof(double));
+            for(int q = 0, qoff = 0; q < inaux; q++, qoff += indim12)
+            {
+                file_->seekp(sizeof(double)*(qoff+ijstart), std::ios_base::beg);
+
+                for(int ij = 0, ijoff = 0; ij < nij; ij++, ijoff += inaux)
+                    file_->write(reinterpret_cast<const char *>(data+ijoff+q), sizeof(double));
             }
         }
         else
         {
-            file_->seekp(sizeof(double)*(ijstart * naux()), std::ios_base::beg);
-            file_->write(reinterpret_cast<const char *>(data), nij*naux()*sizeof(double));
+            file_->seekp(sizeof(double)*(ijstart * inaux), std::ios_base::beg);
+            file_->write(reinterpret_cast<const char *>(data), nij*inaux*sizeof(double));
         }
     }
 }
@@ -70,19 +74,23 @@ void DiskQTensor::WriteByQ_(double * data, int nq, int qstart)
     #pragma omp critical
     #endif
     {
+        int indim12 = ndim12();
+
         if(byq())
         {
-            file_->seekp(sizeof(double)*(qstart*ndim12()), std::ios_base::beg);
-            file_->write(reinterpret_cast<const char *>(data), nq*ndim12()*sizeof(double));
+            file_->seekp(sizeof(double)*(qstart*indim12), std::ios_base::beg);
+            file_->write(reinterpret_cast<const char *>(data), nq*indim12*sizeof(double));
         }
         else
         {
-            for(int q = 0; q < nq; q++)
+            int inaux = naux();
+
+            for(int q = 0, qoff = 0; q < nq; q++, qoff += indim12)
             {
-                for(int ij = 0; ij < ndim12(); ij++)
+                for(int ij = 0, ijoff = 0; ij < indim12; ij++, ijoff += inaux)
                 {
-                    file_->seekp(sizeof(double)*(ij*naux()+qstart+q), std::ios_base::beg);
-                    file_->write(reinterpret_cast<const char *>(data+q*ndim12()+ij), sizeof(double));
+                    file_->seekp(sizeof(double)*(ijoff+qstart+q), std::ios_base::beg);
+                    file_->write(reinterpret_cast<const char *>(data+qoff+ij), sizeof(double));
                 }
             }
         }
@@ -95,20 +103,24 @@ void DiskQTensor::Read_(double * data, int nij, int ijstart)
     #pragma omp critical
     #endif
     {
+        int inaux = naux();
+
         if(byq())
         {
-            for(int q = 0; q < naux(); q++)
-            {
-                file_->seekg(sizeof(double)*(q*ndim12()+ijstart), std::ios_base::beg);
+            int indim12 = ndim12();
 
-                for(int ij = 0; ij < nij; ij++)
-                    file_->read(reinterpret_cast<char *>(data+ij*naux()+q), sizeof(double));
+            for(int q = 0, qoff = 0; q < inaux; q++, qoff += indim12)
+            {
+                file_->seekg(sizeof(double)*(qoff+ijstart), std::ios_base::beg);
+
+                for(int ij = 0, ijoff = 0; ij < nij; ij++, ijoff += inaux)
+                    file_->read(reinterpret_cast<char *>(data+ijoff+q), sizeof(double));
             }
         }
         else
         {
-            file_->seekg(sizeof(double)*(ijstart*naux()), std::ios_base::beg);
-            file_->read(reinterpret_cast<char *>(data), sizeof(double)*nij*naux());
+            file_->seekg(sizeof(double)*(ijstart*inaux), std::ios_base::beg);
+            file_->read(reinterpret_cast<char *>(data), sizeof(double)*nij*inaux);
         }
     }
 }
@@ -119,19 +131,22 @@ void DiskQTensor::ReadByQ_(double * data, int nq, int qstart)
     #pragma omp critical
     #endif
     {
+        int indim12 = ndim12();
+
         if(byq())
         {
-            file_->seekg(sizeof(double)*(qstart*ndim12()), std::ios_base::beg);
-            file_->read(reinterpret_cast<char *>(data), sizeof(double)*nq*ndim12());
+            file_->seekg(sizeof(double)*(qstart*indim12), std::ios_base::beg);
+            file_->read(reinterpret_cast<char *>(data), sizeof(double)*nq*indim12);
         }
         else
         {
-            for(int q0 = 0, q = qstart; q0 < nq; q++, q0++)
-            for(int m = 0; m < ndim1(); m++)
-            for(int n = 0; n < ndim2(); n++)
+            int inaux = naux();
+
+            for(int q = 0, qoff = 0; q < nq; q++, qoff += indim12)
+            for(int ij = 0, ijoff = 0; ij < indim12; ij++, ijoff += inaux)
             {
-                file_->seekg(sizeof(double)*(calcindex(m,n)*naux()+q), std::ios_base::beg);
-                file_->read(reinterpret_cast<char *>(data+q0*ndim12() + calcindex(m,n)), sizeof(double));
+                file_->seekg(sizeof(double)*(ijoff+qstart+q), std::ios_base::beg);
+                file_->read(reinterpret_cast<char *>(data+qoff+ij), sizeof(double));
             }
         }
     }
--- panache/storedqtensor/MemoryQTensor.cc
+++ panache/storedqtensor/MemoryQTensor.cc
@@ -11,68 +11,79 @@ namespace panache
 
 void MemoryQTensor::Write_(double * data, int nij, int ijstart)
 {
+    int inaux = naux();
+
     if(byq())
     {
-        for(int q = 0; q < naux(); q++)
-        for(int ij = 0; ij < nij; ij++)
-            data_[q*ndim12()+ijstart+ij] = data[ij*naux()+q];
+        int indim12 = ndim12();
+
+        for(int q = 0, qoff = 0; q < inaux; q++, qoff += indim12)
+            for(int ij = 0, ijoff = 0; ij < nij; ij++, ijoff += inaux)
+                data_[qoff+ijstart+ij] = data[ijoff+q];
     }
     else
     {
-        double * start = data_.get() + ijstart * naux();
-        std::copy(data, data+naux(), start);
+        double * start = data_.get() + ijstart * inaux;
+        std::copy(data, data+inaux, start);
     }
 }
 
 
 void MemoryQTensor::WriteByQ_(double * data, int nq, int qstart)
 {
+    int indim12 = ndim12();
+
     if(byq())
     {
         std::copy(data,
-                  data + nq*ndim12(),
-                  data_.get()+qstart*ndim12());
+                  data + nq*indim12,
+                  data_.get()+qstart*indim12);
     }
     else
     {
-        for(int q = 0; q < nq; q++)
-        {
-            for(int ij = 0; ij < ndim12(); ij++)
-                data_[ij*naux()+qstart+q] = data[q*ndim12()+ij];
-        }
+        int inaux = naux();
+
+        for(int q = 0, qoff = 0; q < nq; q++, qoff += indim12)
+            for(int ij = 0, ijoff = 0; ij < indim12; ij++, ijoff += inaux)
+                data_[ijoff+qstart+q] = data[qoff+ij];
     }
 }
 
 void MemoryQTensor::Read_(double * data, int nij, int ijstart)
 {
-    // index ij is given by calling function and takes into account packing
+    int inaux = naux();
+
     if(byq())
     {
-        for(int q = 0; q < naux(); q++)
-        for(int ij = 0; ij < nij; ij++)
-            data[ij*naux()+q] = data_[q*ndim12()+ijstart+ij];
+        int indim12 = ndim12();
+
+        for(int q = 0, qoff = 0; q < inaux; q++, qoff += indim12)
+            for(int ij = 0, ijoff = 0; ij < nij; ij++, ijoff += inaux)
+                data[ijoff+q] = data_[qoff+ijstart+ij];
     }
     else
     {
-        double * start = data_.get() + ijstart * naux();
-        std::copy(start, start+nij*naux(), data);
+        double * start = data_.get() + ijstart * inaux;
+        std::copy(start, start+nij*inaux, data);
     }
 }
 
 void MemoryQTensor::ReadByQ_(double * data, int nq, int qstart)
 {
+    int indim12 = ndim12();
+
     if(byq())
     {
-        double * start = data_.get()+qstart*ndim12();
-        std::copy(start, start+nq*ndim12(), data);
+        double * start = data_.get()+qstart*indim12;
+        std::copy(start, start+nq*indim12, data);
     }
     else
     {
-        for(int q0 = 0, q = qstart; q0 < nq; q++, q0++)
-        for(int m = 0; m < ndim1(); m++)
-        for(int n = 0; n < ndim2(); n++)
-            data[q0*ndim12() + calcindex(m,n)]
-                = data_[calcindex(m,n)*naux() + q];
+        int inaux = naux();
+
+        for(int q = 0, qoff = 0; q < nq; q++, qoff += indim12)
+            for(int ij = 0, ijoff = 0; ij < indim12; ij++, ijoff += inaux)
+                data[qoff+ij] = data_[ijoff+qstart+q];
     }
 }
 
